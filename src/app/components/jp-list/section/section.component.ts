9de186060f1ee6adbe2f29381fbfa74c41b06985
nishant <nish54@gmail.com>
2023-09-10 14:41:51 -0500

    section

---
+++ src/app/components/jp-list/section/section.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import { Link, LinkType } from '../typings';
+
+@Component({
+  selector: 'app-section',
+  templateUrl: './section.component.html',
+  styleUrls: ['./section.component.scss']
+})
+export class SectionComponent {
+  GOOGLE_FAVICON_CACHE_URL = 'https://www.google.com/s2/favicons?domain='
+  @Input() links: Array<Link> = [];
+  @Input() heading: LinkType | string = '';
+  getFavicon = (url: string): string => this.GOOGLE_FAVICON_CACHE_URL + url;
+
+}
