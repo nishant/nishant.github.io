c64a76b62868cb9b7e1a8274ebf474d5197dc119
nishant <nish54@gmail.com>
2025-04-13 00:47:35 -0400

    added notes

---
+++ src/app/components/notes/notes.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Editor, Toolbar, Validators } from 'ngx-editor';
+import { FormControl, FormGroup } from '@angular/forms';
+import { NotesService } from './notes.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-notes',
+  templateUrl: './notes.component.html',
+  styleUrls: ['./notes.component.scss']
+})
+export class NotesComponent implements OnInit, OnDestroy {
+  editor: Editor;
+  toolbar: Toolbar = [
+    ['bold', 'italic'],
+    ['underline', 'strike'],
+    ['code', 'blockquote'],
+    ['ordered_list', 'bullet_list'],
+    [{ heading: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'] }],
+    ['link', 'image'],
+    ['text_color', 'background_color'],
+    ['align_left', 'align_center', 'align_right', 'align_justify'],
+  ];
+
+  form = new FormGroup({
+    editorContent: new FormControl({ value: '', disabled: false }, Validators.required()),
+  });
+
+  action$: Subscription;
+
+  constructor(private notesService: NotesService) {
+    this.editor = new Editor({ keyboardShortcuts: true });
+    this.action$ = this.notesService.action.subscribe(action => {
+      if (action === 'save') { this.saveContent(); }
+    });
+  }
+
+  ngOnInit(): void {
+    this.setContent();
+  }
+
+  ngOnDestroy(): void {
+    this.editor?.destroy();
+    this.notesService.action.next('');
+    this.action$.unsubscribe();
+  }
+
+  get content(): string {
+    return <string>this.form.get('editorContent')?.value;
+  }
+
+  logContent = () => console.log(this.content);
+
+  setContent = (): void => {
+    const savedContent = localStorage.getItem('nishant.github.io-notes') ?? '';
+    this.editor?.setContent(savedContent);
+    this.form.get('editorContent')?.setValue(savedContent);
+  }
+
+  saveContent = (): void => localStorage.setItem('nishant.github.io-notes', this.content);
+}
---
+++ src/app/components/notes/notes.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotesService {
+  content = '';
+  action = new BehaviorSubject('');
+
+  constructor() { }
+}
