66c0a5204aa58b1c395a6210e3e7672dd8faba79
nishant <nish54@gmail.com>
2025-04-13 00:45:52 -0400

    WIP stocks

---
+++ src/app/components/stocks/stocks.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-stocks',
+  templateUrl: './stocks.component.html',
+  styleUrls: ['./stocks.component.scss']
+})
+export class StocksComponent {
+  API_KEY = ''
+
+  constructor(private http: HttpClient) { }
+
+  // getStockData()
+}
